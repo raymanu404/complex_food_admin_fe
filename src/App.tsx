51130e1df78a097c7a4ff928ee0e299631fcf956
raymanu404 <manu.caprariu.brediceanu@gmail.com>
2024-04-14 19:42:13 +0300

    config theme in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import { useState } from 'react'
+import { ThemeProvider } from '@emotion/react'
 import './App.css'
+import { theme } from './theme'
+import { Home } from './pages/Home/Home'
+import { StyledEngineProvider } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <ThemeProvider theme={theme}>
+      <StyledEngineProvider injectFirst>
+        {/* use for now this compo, should be router */}
+        <Home />
+      </StyledEngineProvider>
+    </ThemeProvider>
   )
 }
 
 export default App
-
