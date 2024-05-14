608c8a0380c7dc209c628458b6d159592fec1d70
Emanuel Caprariu <emanuel.caprariu@molsondev.com>
2024-05-14 20:57:17 +0300

    put useful comm

--- src/pages/Login/container.tsx
+++ src/pages/Login/container.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Login = (props) => {
   return (
     <>
+    {/* map with router, config auth, get Auth UI provided by supabase, generate magic links for admins to get into app */}
       <h1>Login</h1>
     </>
   )
